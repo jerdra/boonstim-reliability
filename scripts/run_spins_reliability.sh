df47c7da68eca22cfc0bfe6f747dd91219d92724
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2020-04-24 12:17:23 -0400

    update gitignore and remove pilot subject

--- scripts/run_spins_reliability.sh
+++ scripts/run_spins_reliability.sh
@@ -5,5 +5,9 @@ dataset=$repodir/data/datasets/SPINS/bids
 output=$repodir/data/optimization_reliability/SPINS/output
 cache=$repodir/data/optimization_reliability/SPINS/cache
 
+# Make sure target directories exist
+mkdir -p $output
+mkdir -p $cache
+
 # Run BOONStim on SPINS dataset
 $repodir/bin/run_boonstim.sh $dataset $output $cache
